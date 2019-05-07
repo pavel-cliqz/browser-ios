142c1f7fda3e6fef5b240e1e106a0915b1102553
Pavel <pavel@cliqz.com>
2019-05-07 15:26:59 +0200

    [IP-494] [Core] Move API key to binary

---
+++ Cliqz/Resources/APIKeys.swift
@@ -0,0 +1,16 @@
+//
+//  APIKeys.swift
+//  Client
+//
+//  Created by Pavel Kirakosyan on 07.05.19.
+//  Copyright © 2019 Cliqz. All rights reserved.
+//
+
+import Foundation
+
+
+public class APIKeys {
+    public static let sentryDSN: String? = "https://daf932fa43914484898584a43d4fcea8@sentry.io/1327668"
+    public static let lumenAPI: String? = nil
+    public static let revenuecatAPI: String? = nil
+}
