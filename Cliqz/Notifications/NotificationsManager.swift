ab308d20a69474c1c789f3e698643db69503ab00
Pavel <pavel@cliqz.com>
2019-05-14 15:15:16 +0200

    added notificationmanager in project file

--- Cliqz/Notifications/NotificationsManager.swift
+++ Cliqz/Notifications/NotificationsManager.swift
@@ -18,10 +18,47 @@ class NotificationsManager {
     }
     
     func scheduleNotifications() {
-        self.removeAllScheduledNotificaions()
+        if #available(iOS 12.0, *) {
+            let center = UNUserNotificationCenter.current()
+            center.getNotificationSettings { (setting) in
+                if setting.authorizationStatus == .provisional || setting.authorizationStatus == .authorized {
+                    self.removeAllScheduledNotificaions()
+                    
+                    let center = UNUserNotificationCenter.current()
+                    let requests = self.createNotificationReqeusts()
+                    for request in requests {
+                        center.add(request) { (error) in
+                            if let error = error {
+                                print("error \(error.localizedDescription)")
+                            }
+                        }
+                    }
+                } else {
+                    print("notification authorization status - \(setting.authorizationStatus.rawValue)")
+                }
+            }
+        }
     }
     
     func removeAllScheduledNotificaions() {
-        
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
     }
+    
+    // MARK: private methods
+    
+    private func createNotificationReqeusts() -> [UNNotificationRequest] {
+        let currentSubscription = SubscriptionController.shared.getCurrentSubscription()
+        switch currentSubscription {
+        case .limited, .trial(_):
+            return self.createSubscriptionReminderRequests()
+        default:
+            return []
+        }
+    }
+    
+    private func createSubscriptionReminderRequests() -> [UNNotificationRequest] {
+        return []
+    }
+    
 }
