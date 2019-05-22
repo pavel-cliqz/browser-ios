9f5a981a9186b22392deeb39db3f23bc49c0cd2b
Pavel <pavel@cliqz.com>
2019-05-22 12:04:10 +0200

    localized promo condition texts

--- Cliqz/Upgrade&Payment/DataSource/PromoSubscriptionsDataSource.swift
+++ Cliqz/Upgrade&Payment/DataSource/PromoSubscriptionsDataSource.swift
@@ -11,8 +11,10 @@ import Foundation
 class PromoSubscriptionsDataSource {
 	
     private var subscriptionInfo: SubscriptionCellInfo?
+    private var promoType: LumenSubscriptionPromoPlanType
 	
     init(promoType: LumenSubscriptionPromoPlanType, availablePromoSubscription: [LumenSubscriptionProduct]) {
+        self.promoType = promoType
         if let promoProduct = availablePromoSubscription.filter({ $0.product.productIdentifier == promoType.promoID }).first {
             self.subscriptionInfo = SubscriptionCellInfo(priceDetails: nil, offerDetails: self.offerDetails(plan: promoType), isSubscribed: SubscriptionController.shared.hasSubscription(promoProduct.subscriptionPlan), height: 150, lumenProduct: promoProduct)
         }
@@ -34,7 +36,12 @@ class PromoSubscriptionsDataSource {
 	}
 
 	func getConditionText() -> String {
-        return String(format: NSLocalizedString("Payment of %@ will be charged to your Apple ID account each month for 2 months. The subscription automatically renews for %@ per month after 2 months. Subscriptions will be applied to your iTunes account on confirmation. Your account will be charged for renewal within 24 hours prior to the end of the current period at the cost mentioned before. You can cancel anytime in your iTunes account settings until 24 hours before the end of the current period. Any unused portion of a free trial will be forfeited if you purchase a subscription.", tableName: "Lumen", comment: "Lumenconditions for promo"), self.subscriptionInfo?.introductoryPrice ?? "", self.subscriptionInfo?.standardPrice ?? "")
+        switch self.promoType.type {
+        case .half:
+            return String(format: NSLocalizedString("Payment of %@ will be charged to your Apple ID account each month for 2 months. The subscription automatically renews for %@ per month after 2 months. Subscriptions will be applied to your iTunes account on confirmation. Your account will be charged for renewal within 24 hours prior to the end of the current period at the cost mentioned before. You can cancel anytime in your iTunes account settings until 24 hours before the end of the current period. Any unused portion of a free trial will be forfeited if you purchase a subscription.", tableName: "Lumen", comment: "Lumen conditions for 2 months 50% promo"), self.subscriptionInfo?.introductoryPrice ?? "", self.subscriptionInfo?.standardPrice ?? "")
+        case .freeMonth:
+            return String(format: NSLocalizedString("Your Apple ID account will not be charged in the first month. The subscription automatically renews for %@ per month after 1 month. Subscriptions will be applied to your iTunes account on confirmation. Your account will be charged for renewal within 24 hours prior to the end of the current period at the cost mentioned before. You can cancel anytime in your iTunes account settings until 24 hours before the end of the current period. Any unused portion of a free trial will be forfeited if you purchase a subscription.", tableName: "Lumen", comment: "Lumen conditions for 1 month for free"), self.subscriptionInfo?.standardPrice ?? "")
+        }
 	}
     
     
@@ -42,10 +49,9 @@ class PromoSubscriptionsDataSource {
         switch plan.type {
         case .half:
             return NSLocalizedString("GET 2 MONTHS AT 50% OFF", tableName: "Lumen", value:"GET 2 MONTHS \nAT 50% OFF", comment: "GET 2 MONTHS AT 50% OFF")
-        default:
-            break
+        case .freeMonth:
+            return NSLocalizedString("GET 1 MONTH FOR FREE", tableName: "Lumen", value:"GET 1 MONTH \nFOR FREE", comment: "GET 1 MONTH FOR FREE")
         }
-        return nil
     }
 }
 
